f04fe0b7c9ce63d4637cc87f6e42b74f3358fa82
Dante Raphael Ricalde Delgado <dante.ricalde.delgado@gmail.com>
2017-05-30 08:07:25 +0100

    spring initializer web demo

---
+++ spring-initializer-demo-web/src/main/java/com/example/springinitializerdemoweb/SpringInitializerDemoWebApplication.java
@@ -0,0 +1,12 @@
+package com.example.springinitializerdemoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInitializerDemoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInitializerDemoWebApplication.class, args);
+	}
+}

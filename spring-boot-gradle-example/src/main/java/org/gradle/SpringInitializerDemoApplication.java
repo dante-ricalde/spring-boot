567f667d6ad043c50848c626489f43e60f6c8a2a
Dante Raphael Ricalde Delgado <dante.ricalde.delgado@gmail.com>
2017-06-15 08:07:17 +0100

    gradle project example

---
+++ spring-boot-gradle-example/src/main/java/org/gradle/SpringInitializerDemoApplication.java
@@ -0,0 +1,12 @@
+package org.gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInitializerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInitializerDemoApplication.class, args);
+	}
+}

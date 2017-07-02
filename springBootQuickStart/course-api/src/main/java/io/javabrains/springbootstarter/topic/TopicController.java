62e3d91cf087d24f2d4512f73e63b348728fda2c
Dante Ricalde <dante.ricalde.delgado@gmail.com>
2017-07-02 14:05:33 -0500

    implementing update and delete

--- springBootQuickStart/course-api/src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
+++ springBootQuickStart/course-api/src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -1,9 +1,12 @@
 package io.javabrains.springbootstarter.topic;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,12 +17,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TopicController {
 	
+	@Autowired
+	private TopicService topicService;
+	
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {
-		return Arrays.asList(
-				new Topic("spring", "Spring Framework", "Spring Framework Description"),
-				new Topic("java", "Core Java", "Core Java Description"),
-				new Topic("javascript", "Javascript", "Javascript Description")
-				);
+		return topicService.getAllTopics();
+	}
+	
+	@RequestMapping(path = "/topics/{id}", method = RequestMethod.GET)
+	public Topic getTopic(@PathVariable(name = "id") String id) {
+		return topicService.getTopic(id);
+	}
+	
+	@RequestMapping(value = "/topics", method = RequestMethod.POST)
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
+	}
+	
+	@RequestMapping(value = "/topics/{id}", method = RequestMethod.PUT)
+	public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+		topicService.updateTopic(id, topic);
+	}
+	
+	@RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
+	public void deleteTopic(@PathVariable String id) {
+		topicService.deleteTopic(id);
 	}
 }

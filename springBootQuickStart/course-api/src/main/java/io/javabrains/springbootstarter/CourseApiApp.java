0d1efcda4b1eb04ed993fb909da01274336fb1c6
Dante Ricalde <dante.ricalde.delgado@gmail.com>
2017-07-02 08:57:45 -0500

    Spring Boot Quick Start

---
+++ springBootQuickStart/course-api/src/main/java/io/javabrains/springbootstarter/CourseApiApp.java
@@ -0,0 +1,18 @@
+package io.javabrains.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author dante
+ *
+ */
+@SpringBootApplication
+public class CourseApiApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiApp.class, args);
+	}
+
+}
